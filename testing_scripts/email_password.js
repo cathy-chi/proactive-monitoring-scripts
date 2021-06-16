5307d395e38144efb36567efa202c91ae67868d6
cathy-chi <81547950+cathy-chi@users.noreply.github.com>
2021-06-16 14:09:47 -0700

    Update email_password.js

--- testing_scripts/email_password.js
+++ testing_scripts/email_password.js
@@ -12,8 +12,10 @@ const loginAndPassword = async function() {
   await page.goto(
     "https://connect.tryfinch.com/authorize?%20&client_id=3898b9a4-898e-4b82-8abc-a5d719c56bf9%20&products=company%20directory%20&redirect_uri=https://tryfinch.com&sandbox=true&sandbox=true&payroll_provider=" + provider
   );
-  await page.type("[placeholder=Login]", "smallco");
-  await page.type("[placeholder=Password]", "letmein");
+  await page.click("[type=submit]")
+  await page.click("[type=submit]");
+  await page.type("#username", process.env.email);
+  await page.type("#password", process.env.password);
   await page.click("button");
   try { 
     await page.waitForNavigation({ waitUntil: 'load' });

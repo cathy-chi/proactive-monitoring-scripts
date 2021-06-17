d803340bbec35ac9a153a82462c1a1c0d52dba24
cathy-chi <81547950+cathy-chi@users.noreply.github.com>
2021-06-17 13:24:24 -0700

    Update zip.bash

--- yarn_scripts/zip.bash
+++ yarn_scripts/zip.bash
@@ -5,7 +5,8 @@ for file in $FILES
 do
     mkdir -p testing_scripts/nodejs/node_modules/;
     cp $file testing_scripts/nodejs/node_modules/;
-    zip -r $file.zip testing_scripts/nodejs/*;
+    BASENAME=`basename $file .js`
+    zip -r $BASENAME.zip testing_scripts/nodejs/*;
     rm -r testing_scripts/nodejs;
-    mv $file.zip zipped_scripts/
+    mv $BASENAME.zip zipped_scripts/
 done
